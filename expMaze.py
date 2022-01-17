b065b1ee2b76faae98bd261cde5c724f525e60e5
Darren <darrendarren1231@gmail.com>
2022-01-18 05:36:45 +0800

    fix surface display bug

--- expMaze.py
+++ expMaze.py
@@ -10,11 +10,11 @@ import pygame
 import numpy as np
 import sys
 
-def run(n):
+def run(n,maze_h,maze_w):
 
-    surface=create_window()
+    surface=create_window(maze_w,maze_h)
     maxEpisodes=200
-    environment=Environment(6,6)
+    environment=Environment(maze_w,maze_h)
     agent=Q_learning_agent(n,environment)
     
     rlglue=RLGlue(environment,agent,surface)
@@ -25,17 +25,17 @@ def run(n):
         rlglue.rl_episode()
         print("Steps took in this episode was %d" %(rlglue.num_ep_steps()))
 
-def create_window():
+def create_window(maze_w,maze_h):
 
     title = "Dyna Maze"
-    size = (550,370)
+    size = (maze_w*60,maze_h*60)
     pygame.init()
     surface = pygame.display.set_mode(size,0,0)
     pygame.display.set_caption(title)
 
     return surface
 
-run(10)
+run(10,8,8)
 # run(int(sys.argv[1]))
 
 
